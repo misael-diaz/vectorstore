46f966def50ac5280709abb7d9b4454bb33aab02
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-11-17 22:20:29 -0400

    adds document to collection

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 import chromadb
+with open("doc.txt", "r") as f:
+    document = f.read()
+documents = [document]
+ids = [str(i) for i in range(len(documents))]
+client = chromadb.Client()
+collection = client.create_collection(name="collection")
+collection.add(documents=documents, ids=ids)
+results = collection.query(query_texts=["vector database"])
