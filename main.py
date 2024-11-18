e79b2a03e3ee199164095cc47d13b16e958f1bc1
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-11-17 22:42:18 -0400

    reads entire doc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import chromadb
 with open("doc.txt", "r") as f:
-    document = f.readlines()
+    document = f.read()
 documents = [document]
 ids = [str(i) for i in range(len(documents))]
 client = chromadb.Client()
