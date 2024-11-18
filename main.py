dd82b2a7c98b992c0593911af8b79f42dff65577
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-11-18 18:21:20 -0400

    fixes embeddings

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ ids = []
 model = "all-minilm"
 for n, document in enumerate(documents):
     ids.append(str(n))
-    res = ollama.embedding(model=model, prompt=document)
+    res = ollama.embeddings(model=model, prompt=document)
     embedding = res["embedding"]
     embeddings.append(embedding)
 
 client = chromadb.Client()
 collection = client.create_collection(name="collection")
-collection.add(documents=documents, embeddings, ids=ids)
+collection.add(documents=documents, embeddings=embeddings, ids=ids)
 results = collection.query(query_texts=["vector database"])
