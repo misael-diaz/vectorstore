9f7d5b29f645e560879ccebda2fe496a55a248dc
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2024-11-17 22:22:00 -0400

    fixes syntax

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ feature vectors close to each other.
 """
 documents = [document]
 ids = [i for i in range(len(documents))]
-client = chromadb.Client() collection
-= client.create_collection(name="collection")
+client = chromadb.Client()
+collection = client.create_collection(name="collection")
 collection.add(documents=documents, ids=ids)
